a1b11bb871da8a9b508cf0e0e19b875c11e63d6b
andre_zktt <andrezicatti@gmail.com>
2024-10-17 17:34:07 -0300

    Exception handling - update

--- src/main/java/com/andrezktt/project/services/UserService.java
+++ src/main/java/com/andrezktt/project/services/UserService.java
@@ -4,6 +4,7 @@ import com.andrezktt.project.entities.User;
 import com.andrezktt.project.repositories.UserRepository;
 import com.andrezktt.project.services.exceptions.DatabaseException;
 import com.andrezktt.project.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
